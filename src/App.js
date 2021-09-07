af3c9d9de83078c2aa6f03afbd6a01bc964486f8
Emile Thigpen <85521988+ethigpen@users.noreply.github.com>
2021-09-07 14:30:15 -0500

    used an api!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import axios from 'axios';
+
+
 
 function App() {
+
+  const[allPokemon, setAllPokemon]=useState([])
+
+const clickHandler=()=>{
+
+  axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=807")
+  .then(res=>{
+    // console.log(res.data.results)
+    setAllPokemon(res.data.results)
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+
+}
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Click the button for a list of Pokémon</h1>
+      <button onClick={clickHandler} className="btn btn-primary">Fetch Pokémon</button>
+      {
+        allPokemon.map((poke, idx)=>{
+          return <div key={idx}>
+            <h3>{idx}.) {poke.name}</h3>
+          </div>
+
+        })
+      }
     </div>
   );
 }
